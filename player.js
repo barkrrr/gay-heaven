f845e052d693fde843e11cf7a353e08d9db6a7f8
Barbara Koehler <barkoehler@gmail.com>
2018-09-04 13:29:38 -0400

    added a picture remember to resize

--- player.js
+++ player.js
@@ -3,9 +3,12 @@
 function Player(canvas, lives) {
   var self = this;
   
+  self.image = new Image();
+  self.image.src = './images/gay-god.png';
   self.canvasElement = canvas;
   self.lives = lives; 
-  self.size = 50;
+  self.width = 80;
+  self.height = 120;
   self.x = self.canvasElement.width / 2;
   self.y = self.canvasElement.height / 2;
   self.direction = {
@@ -31,10 +34,10 @@ Player.prototype.collidedLive = function () {
 Player.prototype.collidesWithEnemy = function (enemy) {
   var self = this;
 
-  const collidesRight = self.x + self.size / 2 > enemy.x - enemy.size / 2;
-  const collidesLeft = self.x - self.size / 2 < enemy.x + enemy.size / 2;
-  const collidesTop = self.y - self.size / 2 < enemy.y + enemy.size / 2;
-  const collidesBottom = self.y + self.size / 2 > enemy.y - enemy.size / 2;
+  const collidesRight = self.x + self.width / 2 > enemy.x - enemy.size / 2;
+  const collidesLeft = self.x - self.width / 2 < enemy.x + enemy.size / 2;
+  const collidesTop = self.y - self.height / 2 < enemy.y + enemy.size / 2;
+  const collidesBottom = self.y + self.height / 2 > enemy.y - enemy.size / 2;
 
   if (collidesLeft && collidesRight && collidesTop && collidesBottom) {
     self.direction.x = self.direction.x * -1
@@ -62,28 +65,31 @@ Player.prototype.update = function () {
   self.x = self.x + self.direction.x * self.speed;
   self.y = self.y + self.direction.y * self.speed;
 
-  //Check X collisions
-  if (self.x - self.size / 2 < 0) {
-    self.direction.x = 0;
-  };
-  if (self.x + self.size / 2 > self.canvasElement.width) {
-    self.direction.x = 0;
-  };
-
-  //Check Y collisions
-  if (self.y - self.size / 2 < 0) {
-    self.direction.y = 0;
+  //Check X boundaries
+  var minX = self.width / 2;
+  var maxX = self.canvasElement.width - self.width / 2;
+  if (self.x < minX) {
+    self.x = minX;
   }
-  if (self.y + self.size / 2 > self.canvasElement.height) {
-    self.direction.y = 0;
+  if (self.x > maxX) {
+    self.x = maxX;
+  }
+  
+  //Check Y boundaries
+  var minY = self.height / 2
+  var maxY = self.canvasElement.height - self.height / 2;
+  if (self.y < minY) {
+    self.y = minY;
+  }
+  if (self.y > maxY) {
+    self.y = maxY;
   }
 };
 
 Player.prototype.draw = function () {
   var self = this;
 
-  self.ctx.fillStyle = 'pink';
-  var xPosition = self.x - self.size / 2;
-  var yPosition = self.y - self.size / 2;
-  self.ctx.fillRect(xPosition, yPosition , self.size, self.size);
+  var xPosition = self.x - self.width / 2;
+  var yPosition = self.y - self.height / 2;
+  self.ctx.drawImage(self.image, 0, 0, 200, 200, xPosition, yPosition, self.width, self.height);
 };
